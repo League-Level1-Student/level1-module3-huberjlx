82fce45f048a3fefa79d1743a6e211c357e778fe
root <root@6abb98b449d9>
2020-12-29 00:56:27 +0000

    Fixed issue.

--- src/_06_gridworld/gridworld.java
+++ src/_06_gridworld/gridworld.java
@@ -1,15 +1,63 @@
 package _06_gridworld;
 
+import java.awt.Color;
+import java.util.Random;
+
 import info.gridworld.actor.Bug;
 import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class gridworld {
-	
+	public static void main(String[] args) {
+		
 	World world = new World();
+
+	world.show();
+	
 	Bug bug = new Bug();
+	
+	Location location = new Location(0, 0);
+	
+	world.add(location, bug);
+	
+	Random rand = new Random();
+	int numX = rand.nextInt(10);
+	int numY = rand.nextInt(10);
+	
+	Bug bug2 = new Bug();
+	
+	Location location2 = new Location(numX, numY);
+	
+	world.add(location2, bug2);
+	
+	bug2.setColor(Color.BLUE);
+	bug2.turn();
+	bug2.turn();
+	
 	Flower flower = new Flower();
 	
-	world.show();
-	bug.turn();
+	Location location3 = new Location(numX-1, numY);
+	Location location4 = new Location(numX+1, numY);
+	
+	world.add(location3, flower);
+	world.add(location4, flower);
+	
+	int locX = 0;
+	int locY = 0;
+	
+	
+	
+	for (int i = 0; i < 100; i++) {
+		Location loc = new Location(locX, locY);
+		if (locX != numX && locY != numY) {
+			world.add(loc, flower);
+			locX = locX + 1;
+			if (locX == 10) {
+				locX = 0;
+				locY += 1;
+			}
+		}
+	}
+	}
 }
